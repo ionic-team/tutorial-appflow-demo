e76bdca0a599ea391ca5af30fd1a7be09f7805a8
Jon <jon.herman@ionic.io>
2024-01-25 14:28:59 -0500

    add live updates plugin

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,14 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'starter_app',
   webDir: 'www',
+  plugins: {
+    LiveUpdates: {
+      appId: '4d65b71a',
+      channel: 'dev-1.0.0',
+      autoUpdateMethod: 'background',
+      maxVersions: 2,
+    },
+  },
   server: {
     androidScheme: 'https'
   }
